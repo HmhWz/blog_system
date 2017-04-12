49a5391a2bf22b8a60c37cfc1bb4aaf0b2343516
hmh <934178858@qq.com>
2017-04-12 22:31:30 +0800

    some correct

--- new_blog_system/public/js/signup.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$('#signUpName').keydown(function(event){
-		let target = $(event.target);
-		if(target.val().length > 10){
-			console.log('用户名长度不能超过10个字符');
-		}
-	})
-});
--- new_blog_system/routes/signout.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-router.get('/signout', function(req, res, next){
-	req.session.user = null;
-	req.flash('success', '登出成功');
-	return res.redirect('/');
-});
-
-module.exports = router;
